298f6f488dde77f0e635a15d4396986b38c7437a
esquze <rodion.bal@gmail.com>
2025-10-22 15:44:35 +0200

    Fizz-Bearbeitung/Quadrat

--- temp1/PersonTest.java
+++ temp1/PersonTest.java
@@ -1,9 +1,7 @@
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.Assert.assertEquals;
 
 /**
  * The test class PersonTest.
