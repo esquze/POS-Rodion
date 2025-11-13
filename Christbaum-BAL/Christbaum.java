fc99a516e2798793e0c6c7a088f6e0bfb3d3a3a6
esquze <rodion.bal@gmail.com>
2025-11-13 23:44:32 +0100

    ChristbaumAufgabe

---
+++ Christbaum-BAL/Christbaum.java
@@ -0,0 +1,20 @@
+public class Christbaum
+{
+    public static void zeichne(int n) {
+        for (int z = 0; z < n; z++) {
+            int r = n - 1 - z;
+            for (int i = 0; i < r; i++) {
+                System.out.print(" ");
+            }    
+            System.out.print("[");
+            
+            int st = z * 2 + 1;
+            for (int i = 0; i < st; i++) {
+                System.out.print("*");  
+            }
+                
+            System.out.print("]");
+            System.out.println();
+        }
+    }
+}
